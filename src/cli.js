c6513c42506e5d6a413a387fde6a2a1423cb210f
Konstantin Freidlin <freidlinks@ya.ru>
2022-07-04 19:34:15 +0300

    Update cli, create simple test data

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,20 @@
 import { program } from 'commander';
 
-program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+/**
+ * Parse CLI arguments
+ * @returns {string[]}
+ */
+export default () => {
+  program
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0')
+    .option('-f, --format <format>', 'set formatters of output', 'stylish')
+    .argument('<filepath1>')
+    .argument('<filepath2>');
 
-program.option('-V', '--version', 'output ther version number')
-program.option('-h', '--help', 'display help for command')
-
-
-program.parse();
+  program.parse();
+  const [filepath1, filepath2] = program.args;
+  const format = program.opts().format;
+  return [filepath1, filepath2, format]
+}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import parse_args from './cli.js'
+
+export default() => console.log('Hello')
