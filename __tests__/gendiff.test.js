df2c42a61e5f794f30f048423d37d2e447b17437
Konstantin Freidlin <freidlinks@ya.ru>
2022-07-28 13:54:46 +0300

    Update tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,25 +1,25 @@
 import fs from 'fs';
+import path from 'path';
 
 import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
 
+const getFixturePath = (fixtureName, format) => path.join(`./__fixtures__/${fixtureName}.${format}`)
 const readFixture = (path) => fs.readFileSync(path, { encoding: 'utf-8', flag: 'r' });
 
-const jsonSecondCaseInputFilepath1 = './__fixtures__/file1.json';
-const jsonSecondCaseInputFilepath2 = './__fixtures__/file2.json';
-const ymlSecondCaseInputFilepath1 = './__fixtures__/file1.json';
-const ymlSecondCaseInputFilepath2 = './__fixtures__/file2.json';
-const outputSecondCaseJsonFormat = readFixture('./__fixtures__/json-diff.json');
-const outputSecondCasePlainFormat = readFixture('./__fixtures__/plain-diff.txt');
-const outputSecondCaseStylishFormat = readFixture('./__fixtures__/stylish-diff.txt');
+const outputJsonFormat = readFixture('./__fixtures__/json-diff.json');
+const outputPlainFormat = readFixture('./__fixtures__/plain-diff.txt');
+const outputStylishFormat = readFixture('./__fixtures__/stylish-diff.txt');
 
 test.each([
-  [jsonSecondCaseInputFilepath1, jsonSecondCaseInputFilepath2, 'json', outputSecondCaseJsonFormat],
-  [ymlSecondCaseInputFilepath1, ymlSecondCaseInputFilepath2, 'json', outputSecondCaseJsonFormat],
-  [jsonSecondCaseInputFilepath1, jsonSecondCaseInputFilepath2, 'plain', outputSecondCasePlainFormat],
-  [ymlSecondCaseInputFilepath1, ymlSecondCaseInputFilepath2, 'plain', outputSecondCasePlainFormat],
-  [jsonSecondCaseInputFilepath1, jsonSecondCaseInputFilepath2, 'stylish', outputSecondCaseStylishFormat],
-  [ymlSecondCaseInputFilepath1, ymlSecondCaseInputFilepath2, 'stylish', outputSecondCaseStylishFormat],
-])('.gendiff(%s, %s, %s)', (filepath1, filepath2, style, expected) => {
+  ['json', 'json', outputJsonFormat],
+  ['yml', 'json', outputJsonFormat],
+  ['json', 'plain', outputPlainFormat],
+  ['yml', 'plain', outputPlainFormat],
+  ['json', 'stylish', outputStylishFormat],
+  ['yml', 'stylish', outputStylishFormat],
+])('.gendiff(%s, %s, %s)', (fileFormat, style, expected) => {
+  const filepath1 = getFixturePath('file1', fileFormat);
+  const filepath2 = getFixturePath('file2', fileFormat);
   expect(genDiff(filepath1, filepath2, style)).toBe(expected);
 });
